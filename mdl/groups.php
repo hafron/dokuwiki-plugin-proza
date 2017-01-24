83152716ec2dfb5cc9ee873cb83d60657b87c881
Víctor Corbacho <victorcorbacho@iternova.net>
2017-01-24 10:11:05 +0100

    Fixed error when using plugin in dokuwiki configured in spanish language. Added language identifier to mdl/groups.php file.

--- mdl/groups.php
+++ mdl/groups.php
@@ -7,7 +7,8 @@ class Proza_Groups extends Proza_Table {
 	public $fields = array(
 			'id'	=> array('INTEGER', 'NOT NULL', 'PRIMARY KEY'),
 			'pl'	=> array('TEXT', 'NOT NULL'),
-			'en'	=> array('TEXT', 'NOT NULL')
+			'en'	=> array('TEXT', 'NOT NULL'),
+			'es'	=> array('TEXT', 'NOT NULL')
 		);
 	function __construct($db) {
 		$helper = plugin_load('helper', 'proza');
@@ -16,6 +17,7 @@ class Proza_Groups extends Proza_Table {
 	function insert($post) {
 		$post['pl'] = trim($post['pl']);
 		$post['en'] = trim($post['en']);
+		$post['es'] = trim($post['es']);
 		parent::insert($post);
 	}
 
@@ -24,7 +26,7 @@ class Proza_Groups extends Proza_Table {
 		if ($id > -1) 
 			$where = "WHERE id=$id";
 
-		return $this->db->query("SELECT id, pl, en,
+		return $this->db->query("SELECT id, pl, en, es,
 								(SELECT COUNT(*) FROM events WHERE group_n=groups.id) AS refs
 								FROM groups $where");	
 	}
@@ -41,7 +43,7 @@ class Proza_Groups extends Proza_Table {
 	}
 
 	function groups($lang_code='en') {
-		if (!in_array($lang_code, array('en', 'pl')))
+		if (!in_array($lang_code, array('en', 'pl', 'es')))
 			throw new Proza_DBException("unknown column: $lang_code in groups");
 		$r = $this->db->query("SELECT id, $lang_code FROM groups");
 		$groups = array();
